eafe09510776723e2446dd24e7159096f5fc8b6d
taimek <waszkitymek@gmail.com>
2025-05-29 20:28:40 +0000

    did a protoprotoype of the game ;3

--- script.js
+++ script.js
@@ -11,17 +11,25 @@ const openupgradeshop = document.getElementById("openupgradeshop");
 const moneyamount = document.getElementById("moneyamount");
 const carrotfieldarea = document.getElementById("carrotfieldplace");
 const allCarrotFields = document.querySelectorAll('.field');
-marchewka.src = "marchewka.png";
+const mainmenu = document.getElementById("mainmenu");
+const skilltree = document.getElementById("skilltree");
+const openskilltree = document.getElementById("openskilltree");
+const closeskilltree = document.getElementById("closeskilltree");
+const xpamount = document.getElementById("xpamount");
+const getxp = document.getElementById("getxp");
 let priceupgrade1 = 10;
 let priceupgrade2 = 100;
 let priceupgrade3 = 1;
 let marchewki = 0;
 const marchewkifieldamt = []; 
 let marchewkimulti = 1;
-let money = 15 ;
+let money = 150000 ;
 let marchewkibase = 1;
 let fieldnumber = 0;
-
+let marchewkiprice = 2;
+let xp = 0;
+let skill1amountbought = 0;
+let skill1price = 1;
 
 function collectmarchewka(event) {
     const index = parseInt(event.currentTarget.dataset.index, 10);
@@ -49,8 +57,10 @@ function updateui() {
     setInterval(() => {
     carrotsamount.textContent = "You Have: " + marchewki +" Carrots";
     moneyamount.textContent = "You Have: " + money + " Money";
+    xpamount.textContent = "You Have: " + xp + " XP";
     functional_shopsell();
     functional_upgradeshop();
+    functional_skilltree();
     document.querySelectorAll(".carrotamountinfield").forEach((elem, index) => {
   elem.textContent = `Carrots in field: ${marchewkifieldamt[index]}`;
 });
@@ -62,6 +72,8 @@ function updateui() {
         document.getElementById("upgrade2").textContent = "Upgrade 2: " + priceupgrade2 + " Money";
          document.getElementById("upgrade3").textContent = "Buy new Plots!: " + priceupgrade3 + " Money";
     }
+    document.getElementById("skill1").textContent = "Doubles price of carrots Price: " + skill1price + `xp.    ` + skill1amountbought + "/10" + " bought";
+        
 }, 10)}  ;
 
 function loadsellshop() {
@@ -75,12 +87,29 @@ function loadsellshop() {
         sellshop.classList.remove("hide");
         sellshop.classList.add("show");
         const sellcarrotsbutton = document.getElementById("sellcarrots");
+        const amountxptoconvert = document.getElementById("amountxptoconvert");
+        const convertmoneytoxp = document.getElementById("convertmoneytoxp");
         sellcarrotsbutton.addEventListener("click", () => { 
             if (marchewki > 0) {
-              money += marchewki * 2;
+              money += marchewki * marchewkiprice;
               marchewki = 0;       
             }
           });
+          convertmoneytoxp.addEventListener("click", () => {
+            if(amountxptoconvert.value > 0 && money >= parseInt(amountxptoconvert.value, 10)*10000){
+                xp += parseInt(amountxptoconvert.value, 10);
+                money -= parseInt(amountxptoconvert.value, 10)*10000;
+                amountxptoconvert.value = "";
+            }
+            else{
+                alert("You dont have enough money!!!");
+            }
+      });
+
+
+
+
+
 
       })
       .catch(error => {
@@ -142,7 +171,55 @@ function loadupgradeshop() {
         upgradeshop.textContent = "Błąd: " + error.message;
       });
 
+}
+
+function loadskilltree() {
+    fetch("skilltree.html")
+      .then(response => {
+        if (!response.ok) throw new Error("Błąd ładowania");
+        return response.text();
+      })
+      .then(html => {
+        skilltree.innerHTML = html;
+        skilltree.classList.remove("hide");
+        skilltree.classList.add("show");
+         if(mainmenu.classList.contains("show")) {
+        mainmenu.classList.remove("show");
+        mainmenu.classList.add("hide");
+}
+
+        const skill1 = document.getElementById("skill1");
+        skill1.addEventListener("click", () => {
+            if(skill1amountbought < 10 && xp >= skill1price){
+                skill1amountbought += 1;
+                xp -= skill1price;
+                marchewkiprice *= 2;
+                skill1price *= 5;
+            }
+            else {
+                alert("You dont have enough XP or you reached max level of this skill!");
+            }
+    });
+
+
+
+      })
+      .catch(error => {
+        skilltree.textContent = "Błąd: " + error.message;
+      });
+
   }
+function close_skilltree() {
+    if(skilltree.classList.contains("show")) {
+        skilltree.classList.remove("show");
+        skilltree.classList.add("hide");
+        
+      }
+    if(mainmenu.classList.contains("hide")) {
+        mainmenu.classList.remove("hide");
+        mainmenu.classList.add("show");
+    }
+}
 
 function closesellshop() {
     if(sellshop.classList.contains("show")) {
@@ -185,6 +262,22 @@ function functional_upgradeshop(){
     else {
         closeupgradeshop.classList.remove("hide");
     }
+}
+function functional_skilltree(){
+    if(skilltree.classList.contains("show")) {
+        openskilltree.classList.add("hide");
+    }
+    else {
+        openskilltree.classList.remove("hide");
+    }
+    if(skilltree.classList.contains("hide")) {
+        closeskilltree.classList.add("hide");
+    }
+    else {
+        closeskilltree.classList.remove("hide");
+    }
+    
+   
 }
 
 updateui();
